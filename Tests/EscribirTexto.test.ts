1cefa16c465a73408b42962ade91a22deeeeb458
nacho rosales <nacho.rosales04@gmail.com>
2025-04-14 12:40:26 -0300

    test funcional

---
+++ Tests/EscribirTexto.test.ts
@@ -0,0 +1,32 @@
+import { Editor } from "../EditorDeTexto";
+import { Historial } from "../Historial";
+
+describe("Editor con patrón Memento", () => {
+    let editor: Editor;
+    let historial: Historial;
+  
+    beforeEach(() => {
+      historial = new Historial();
+      editor = new Editor(historial);
+    });
+    
+    test("Escribir texto y deshacer cambios", () => {
+        //Cada vez que escribimos, se guarda automaticamente el estado anterior
+        editor.escribir("Hola");
+        editor.escribir(" mundo");
+        editor.escribir("!!!");
+        
+        
+        expect(editor.obtenerContenido()).toBe ("Hola mundo!!!");
+
+//Deshacer una vez
+const estado1 = historial.deshacer();
+if (estado1) editor.restaurar(estado1);
+expect(editor.obtenerContenido()).toBe("Hola mundo");
+
+//Deshacer otra vez
+const estado2 = historial.deshacer();
+    if (estado2) editor.restaurar(estado2);
+    expect(editor.obtenerContenido()).toBe("Hola");
+  });
+});
